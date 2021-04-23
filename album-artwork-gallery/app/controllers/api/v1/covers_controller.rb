7eda3e1b2c59896068324354649dc4cb0d7d4f89
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-04-23 19:11:45 -0400

    create action and strong params for cover controller

--- album-artwork-gallery/app/controllers/api/v1/covers_controller.rb
+++ album-artwork-gallery/app/controllers/api/v1/covers_controller.rb
@@ -5,6 +5,19 @@ def index
     render json: covers
 end
 
+def create
+    cover = Cover.new(cover_params)
+    if cover.save
+        render json: cover, status: :accepted    
+    else
+        render json: {errors: cover.errors.full_message}, status: :unprocessible_entity
+    end
+end
 
+private
+
+def cover_params
+    params.require(cover).permit(:image_url, :album_id, :stars)
+end
 
 end
