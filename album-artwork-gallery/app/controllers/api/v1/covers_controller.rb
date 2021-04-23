8d05d943dd4ba86a7d84a06666c8729e0c6cede7
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-04-23 18:52:18 -0400

    api covers controller generated

---
+++ album-artwork-gallery/app/controllers/api/v1/covers_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::CoversController < ApplicationController
+end
---
+++ album-artwork-gallery/test/controllers/api/v1/covers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::V1::CoversControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
