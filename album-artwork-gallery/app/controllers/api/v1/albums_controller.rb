a08d7fcd710332d323df8fcd1c8c2dbdd8b7ec9c
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-04-24 18:27:56 -0400

    added album creation functionality  with controller action and route, also hard coded in some seed data info

--- album-artwork-gallery/app/controllers/api/v1/albums_controller.rb
+++ album-artwork-gallery/app/controllers/api/v1/albums_controller.rb
@@ -2,5 +2,20 @@ class Api::V1::AlbumsController < ApplicationController
     def index
         albums = Album.all
         render json: AlbumSerializer.new(albums)
-      end
+    end
+
+    def create
+        album = Album.new(album_params) 
+        if album.save
+        render json: AlbumSerializer.new(album)
+        else
+            render json: {errors: album.errors.full_message}
+        end
+    end
+
+    private
+
+    def album_params
+        params.require(:album).permit(:title, :tracks, :artist, :origin)   
+    end
 end
--- album-artwork-gallery/db/seeds.rb
+++ album-artwork-gallery/db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 albums = Album.create([{origin: 'a mixture of braggadocio and emotional lyrics, exploring themes of fame, romance, and wealth.', artist: 'Drake', title: 'Take Care', tracks: 20}, {origin: 'a bunch of producers coming in, a bunch of rappers coming in, singers, managers, you name it. If you pop in one room and theyre making trap music, you pop in another room and theyre making some R&B, pop in another room and theyre making some soulful stuff. You never know.', artist:'J.Cole and Dreamville', title: 'Revenge of the Dreamers 3', tracks: 30} ])
-Cover.create(image_url: 'https://hiphop-n-more.com/wp-content/uploads/2020/01/dreamville-deluxe-1200x1200.jpg', album_id: albums.last.id, stars: 4)
+Cover.create(image_url: 'https://hiphop-n-more.com/wp-content/uploads/2020/01/dreamville-deluxe-1200x1200.jpg', album_id: 2, stars: 4)
